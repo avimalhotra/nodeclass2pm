3cdd6b39da490c610badfeb8462e4efd1a5866a6
Avinash Malhotra <avimalhotra505@gmail.com>
2021-01-24 15:03:46 +0530

    Express JS get and Post

--- src/server.js
+++ src/server.js
@@ -1,49 +1,60 @@
-const http=require('http');
-const ip='127.0.0.1';
-const port=8080;
-const fs=require('fs');
+const env=require("dotenv").config();
+const express=require('express');
+const app=express();
 
 
-/* http.createServer((req,res)=>{
-    res.end("Hello HTTP");
-}).listen(80); */
+app.use( express.static('src/public'));
 
-const server=http.createServer( (req,res)=>{
-    //res.statusCode=200;
-    //res.setHeader('Content-Type','text/html');
-    res.writeHead(200,{'Content-Type':'text/html'});
- 
-    /*
-    res.write("<h1>");
-    //res.write(ip+":"+port);
-    //res.write(req.url);
-    //res.write(req.method);
-    res.write("Hello Http");
-    res.write("</h1>");
-    */
-    if( req.method=="GET" && req.url=="/"){
-        //res.write("Home Page");
-        fs.readFile('src/index.html',(err,data)=>{
-
-            if( err){
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write("Error found");
-                res.end();
-            }
-            else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write(data);
-                res.end();
-            }
-        });
-    }
-    else{
-        res.write("Error");
-        res.end();
-    }
-    
+app.use((req,res,next)=>{
+    console.log('Time: %d', Date.now());
+    next();
 });
 
-server.listen(port,ip,()=>{
-    console.log(`Server running at http://${ip}:${port}`);
+
+app.get('/',(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send('<h1>Hello Express</h1>');
+   
+
+});
+app.get('/app',(req,res)=>{
+    res.status(200).send(" Application running");
+});
+
+/* Get */
+app.get('/getmonth',(req,res)=>{
+    
+    var data=req.query.search;
+    var month;
+    switch(data){
+        case '1':  month="jan"; break;
+        case '2':  month="feb"; break;
+        case '3':  month="mar"; break;
+        default:  month="invalid month"; break;
+    }
+
+    res.status(200).send(month);
+});
+app.get('/getform',(req,res)=>{
+    var formdata=req.query;
+    res.status(200).send(formdata);
+    //res.status(200).json( {data:formdata} );
+});
+app.get('/product/:name/:model',(req,res)=>{
+    res.status(200).send(req.params);
+})
+
+app.post('/postdata',(req,res)=>{
+    //res.status(200).send(`Posted`);
+    res.status(200).send(req.query);
+});
+
+
+/* Wild card handler */
+app.get('/**',(req,res)=>{
+    res.status(404).send(" <h1> 404, Page Not Found </h1>");
+});
+
+app.listen(process.env.PORT,process.env.IP,()=>{
+    console.log(`Express Server running at http://${process.env.IP}:${process.env.PORT}`);
 });
